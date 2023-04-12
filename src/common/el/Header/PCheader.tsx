db35c6c784b2ab91e2831a1d1db300077992760e
leehojun0314 <dlghwns0314@naver.com>
2023-04-12 16:11:35 +0900

    cookie test 2

--- src/common/el/Header/PCheader.tsx
+++ src/common/el/Header/PCheader.tsx
@@ -44,16 +44,16 @@ export const Pcheader = () => {
 		// 			setAuth(err.response.data.isLoggedIn);
 		// 		}
 		// 	});
-		// axiosAPI({
-		// 	method: 'GET',
-		// 	url: '/auth',
-		// })
-		// 	.then((response) => {
-		// 		console.log('response: ', response);
-		// 	})
-		// 	.catch((err) => {
-		// 		console.log('err: ', err);
-		// 	});
+		axiosAPI({
+			method: 'GET',
+			url: '/auth',
+		})
+			.then((response) => {
+				console.log('response: ', response);
+			})
+			.catch((err) => {
+				console.log('err: ', err);
+			});
 	}, []);
 
 	const navModels = [
