aa54d8200988681ce8cf1a680fa1146b2017f3c7
leehojun0314 <dlghwns0314@naver.com>
2023-04-25 16:45:10 +0900

    change texts of upload page

--- src/domain/Upload/UploadView.tsx
+++ src/domain/Upload/UploadView.tsx
@@ -1,30 +1,36 @@
-import { PcFooter } from "@/common/el/footer/PcFooter";
-import { useCustomMediaQuery } from "@/common/theme/screen";
-import { css } from "@emotion/react";
+import { PcFooter } from '@/common/el/footer/PcFooter';
+import { useCustomMediaQuery } from '@/common/theme/screen';
+import { css } from '@emotion/react';
 
 import {
-  FirstSection,
-  MainSection,
-  MobileMainSection,
-  SecondSection,
-  ThirdSection,
-} from "./sections";
-
+	FirstSection,
+	MainSection,
+	MobileMainSection,
+	SecondSection,
+	ThirdSection,
+} from './sections';
+import texts from './model/texts';
+export type Ttext = {
+	bigTitle: string;
+	rightText: string;
+	leftTitle: string;
+	leftText: string;
+};
 export const UploadView = () => {
-  const { isSmall } = useCustomMediaQuery();
-  return (
-    <div css={sx.root}>
-      {isSmall ? <MobileMainSection /> : <MainSection />}
-      <FirstSection />
-      <SecondSection />
-      <ThirdSection />
-      <PcFooter position="relative" />
-    </div>
-  );
+	const { isSmall } = useCustomMediaQuery();
+	return (
+		<div css={sx.root}>
+			{isSmall ? <MobileMainSection /> : <MainSection />}
+			<FirstSection text={texts[0]} />
+			<SecondSection text={texts[1]} />
+			<ThirdSection text={texts[2]} />
+			<PcFooter position='relative' />
+		</div>
+	);
 };
 
 const sx = {
-  root: css`
-    height: 100%;
-  `,
+	root: css`
+		height: 100%;
+	`,
 };
--- src/domain/Upload/sections/ThirdSection.tsx
+++ src/domain/Upload/sections/ThirdSection.tsx
@@ -1,24 +1,24 @@
-import { Mq } from "@/common/theme/screen";
-import { css } from "@emotion/react";
-import { SectionLayout1 } from "../el/SectionLayout1";
+import { Mq } from '@/common/theme/screen';
+import { css } from '@emotion/react';
+import { SectionLayout1 } from '../el/SectionLayout1';
+import { Ttext } from '../UploadView';
 
-export const ThirdSection = () => {
-  const desc = "원하는 질문과 파일은\n무엇이든 AI 채팅이 해결해줍니다.";
-  return (
-    <div>
-      <div css={sx.inner}>
-        <SectionLayout1 desc={desc} />
-      </div>
-    </div>
-  );
+export const ThirdSection = ({ text }: { text: Ttext }) => {
+	return (
+		<div>
+			<div css={sx.inner}>
+				<SectionLayout1 text={text} />
+			</div>
+		</div>
+	);
 };
 
 const sx = {
-  inner: css`
-    width: 1000px;
-    margin: 0 auto;
-    @media ${Mq.xl} {
-      width: 90%;
-    }
-  `,
+	inner: css`
+		width: 1000px;
+		margin: 0 auto;
+		@media ${Mq.xl} {
+			width: 90%;
+		}
+	`,
 };
