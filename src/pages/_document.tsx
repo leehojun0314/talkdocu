8846cb182b4f597867beda76772bebbc24465824
leehojun0314 <dlghwns0314@naver.com>
2023-05-26 00:12:11 +1000

    fix header script of google adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,7 +28,7 @@ export default function Document() {
 				<script
 					async
 					crossOrigin='anonymous'
-					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7554551043921031'
 				></script>
 				<link
 					rel='stylesheet'
