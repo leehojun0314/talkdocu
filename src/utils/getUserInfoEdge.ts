a674f1c100fb26419267a34bc5afcc4cd07f94d1
leehojun0314 <dlghwns0314@naver.com>
2023-12-26 01:45:28 +1000

    log jwt

--- src/utils/getUserInfoEdge.ts
+++ src/utils/getUserInfoEdge.ts
@@ -2,9 +2,12 @@ import { extractToken } from './extractToken';
 import * as jose from 'jose';
 export async function getUserInfoEdge(request: Request) {
 	const jwt = extractToken(request.headers.get('cookie'));
+	console.log('jwt: ', jwt);
 	const key = new TextEncoder().encode(process.env.NEXTAUTH_SECRET);
+	console.log('key: ', key);
 	const { payload } = await jose.jwtVerify(String(jwt), key, {
 		algorithms: ['HS256'],
 	});
+	console.log('payload:', payload);
 	return payload;
 }
