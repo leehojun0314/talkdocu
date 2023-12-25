f14a48cf872c9b2eeaf86d868d16128c18596420
leehojun0314 <dlghwns0314@naver.com>
2023-12-25 20:54:29 +1000

    edge + serverless

---
+++ src/pages/api/ai/streamMessage.ts
@@ -0,0 +1,59 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {
+	OpenAIStream,
+	StreamingTextResponse,
+	experimental_StreamData,
+} from 'ai';
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { extractToken } from '@/utils/functions';
+import jose from 'jose';
+import { decode } from 'next-auth/jwt';
+type Data = {
+	name: string;
+};
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+export const runtime = 'edge';
+export default async function POST(request: Request) {
+	console.log('get called');
+
+	try {
+		const jwt = extractToken(request.headers.get('cookie'));
+		console.log('cookie: ', jwt);
+		const decoded = await decode({
+			token: jwt ?? '',
+			secret: process.env.NEXTAUTH_SECRET ?? '',
+		});
+
+		console.log('decoded: ', decoded);
+		const res = await openai.createChatCompletion({
+			model: 'gpt-4-1106-preview',
+			messages: [
+				{
+					role: 'user',
+					content: '안녕 내 이름은 이호준이야.',
+				},
+			],
+			temperature: 0.7,
+			stream: true,
+		});
+		const data = new experimental_StreamData();
+		data.append({ test: 'data' });
+		const stream = OpenAIStream(res, {
+			onFinal(completion) {
+				data.close();
+			},
+			experimental_streamData: true,
+		});
+
+		const dataObj = JSON.stringify({ test: 'data' });
+		// console.log('data: ', data);
+		return new StreamingTextResponse(stream, undefined, data);
+	} catch (error) {
+		console.log('error: ', error);
+		return new Error('Error occured');
+	}
+}
--- src/pages/api/aitest.ts
+++ src/pages/api/aitest.ts
@@ -1,48 +1,101 @@
-// import type { NextRequest } from 'next/server';
-// import { Configuration, OpenAIApi } from 'openai-edge';
-// import { Readable } from 'stream';
+import type { NextRequest } from 'next/server';
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { Readable } from 'stream';
 
-// const configuration = new Configuration({
-// 	apiKey: process.env.OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-// export const runtime = 'edge';
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+export const runtime = 'edge';
 
-// const POST = async (req: Response) => {
-// 	try {
-// 		const completion = await openai.createChatCompletion({
-// 			model: 'gpt-3.5-turbo',
-// 			messages: [
-// 				{ role: 'user', content: 'Who won the world series in 2020?' },
-// 			],
-// 			max_tokens: 1024,
-// 			temperature: 0,
-// 			stream: true,
-// 		});
-// 		console.log('completion body: ', completion.body);
-// 		return new Response(completion.body, {
-// 			headers: {
-// 				'Access-Control-Allow-Origin': '*',
-// 				'Content-Type': 'text/event-stream;charset=utf-8',
-// 				'Cache-Control': 'no-cache, no-transform',
-// 				'X-Accel-Buffering': 'no',
-// 				additional: 'test data',
-// 			},
-// 		});
-// 	} catch (error: any) {
-// 		console.error(error);
+const POST = async (req: Response) => {
+	try {
+		const completion = await openai.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages: [
+				{ role: 'user', content: 'Who won the world series in 2020?' },
+			],
+			max_tokens: 1024,
+			temperature: 0,
+			stream: true,
+		});
+		console.log('completion body: ', completion.body);
+		if (!completion.body) {
+			return;
+		}
+		const reader = completion.body.getReader();
 
-// 		return new Response(JSON.stringify(error), {
-// 			status: 400,
-// 			headers: {
-// 				'content-type': 'application/json',
-// 			},
-// 		});
-// 	}
-// };
+		// 사용자 정의 객체
+		const myData = { additionalData: 'My custom data' };
 
-// // export const config = {
-// // 	runtime: 'edge',
-// // };
+		const newStream = new ReadableStream({
+			async start(controller) {
+				let responseText = '';
+				let combinedData = { ...myData }; // 사용자 정의 데이터를 미리 포함
 
-// export default POST;
+				while (true) {
+					const { done, value } = await reader.read();
+					if (done) break;
+
+					let textChunk = new TextDecoder('utf-8').decode(value, {
+						stream: !done,
+					});
+
+					// "data: " 접두사 제거
+					// textChunk = textChunk.replace(/^data: /, '');
+					console.log('textChunk: ', textChunk);
+					const dataArr = textChunk.split('\n\n');
+
+					for (let stringData of dataArr) {
+						console.log('stringData: ', stringData);
+						const parsedData = JSON.parse(stringData);
+						console.log('parsedData: ', parsedData);
+					}
+					// console.log('text chunk :', textChunk);
+					try {
+						// 각 textChunk를 개별적으로 JSON 객체로 변환
+						const chunkData = JSON.parse(textChunk);
+						console.log('chunk data:', chunkData);
+						// 필요한 정보를 combinedData 객체에 추가
+						// 예: id, choices 등을 추가하는 방식으로
+						combinedData = { ...combinedData, ...chunkData };
+					} catch (e) {
+						console.error('Error parsing chunk: ', e);
+					}
+				}
+
+				try {
+					// 최종적으로 조합된 데이터를 JSON 문자열로 변환
+					const newResponseText = JSON.stringify(combinedData);
+
+					// 새 스트림에 기록
+					controller.enqueue(new TextEncoder().encode(newResponseText));
+				} catch (e) {
+					controller.error(e);
+				}
+
+				controller.close();
+			},
+		});
+
+		return new Response(newStream, {
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'application/json',
+				'Cache-Control': 'no-cache, no-transform',
+				'X-Accel-Buffering': 'no',
+			},
+		});
+	} catch (error: any) {
+		console.error(error);
+
+		return new Response(JSON.stringify(error), {
+			status: 400,
+			headers: {
+				'content-type': 'application/json',
+			},
+		});
+	}
+};
+
+export default POST;
