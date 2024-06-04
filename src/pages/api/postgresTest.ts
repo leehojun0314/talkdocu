f6dedcccff8b12e68311f06b4675ee6e73eba03e
leehojun0314 <dlghwns0314@naver.com>
2024-06-05 09:59:59 +1000

    remove postgres

--- src/pages/api/postgresTest.ts
+++ src/pages/api/postgresTest.ts
@@ -1,16 +1,16 @@
-import { sql } from '@vercel/postgres';
-export const runtime = 'edge';
-export default async function GET(request: Request) {
-	try {
-		const result =
-			await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
-		return new Response(JSON.stringify(result), {
-			status: 200,
-		});
-	} catch (error) {
-		console.log('error: ', error);
-		return new Response(JSON.stringify(error), {
-			status: 500,
-		});
-	}
-}
+// import { sql } from '@vercel/postgres';
+// export const runtime = 'edge';
+// export default async function GET(request: Request) {
+// 	try {
+// 		const result =
+// 			await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
+// 		return new Response(JSON.stringify(result), {
+// 			status: 200,
+// 		});
+// 	} catch (error) {
+// 		console.log('error: ', error);
+// 		return new Response(JSON.stringify(error), {
+// 			status: 500,
+// 		});
+// 	}
+// }
