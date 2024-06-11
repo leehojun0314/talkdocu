a89dcb81dde1f5bee15ed03774ec9f92a527095a
leehojun0314 <dlghwns0314@naver.com>
2024-06-11 16:59:03 +1000

    increase prompt size for related paragraph

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import { ConstructorOptions } from 'audiomotion-analyzer';
 
 export const configs = {
 	authenticateUrl: 'https://dtizen-secure.vercel.app',
-	relatedParagraphLength: 3000,
+	relatedParagraphLength: 10000,
 	createSalutationLength: 3000,
 	salutationPrefixMessage: 'Hello! How can I help you?',
 	vectorResultSimilarityScore: 0.75,
