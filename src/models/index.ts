83e9515d2ab5fad5b393b762f5f94867a8c0bee4
leehojun0314 <dlghwns0314@naver.com>
2024-09-26 12:41:16 +1000

    fix create conversation

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,152 +1,153 @@
 import { configs } from '@/config';
 import {
-	TExtendedSession,
-	TProvider,
-	TStatus,
-	TUserFromDB,
+  TExtendedSession,
+  TProvider,
+  TStatus,
+  TUserFromDB,
 } from '@/types/types';
 import mssql from 'mssql';
 export const sqlConnectionPool = new mssql.ConnectionPool({
-	user: process.env.DB_USER ?? '',
-	password: process.env.DB_PWD ?? '',
-	database: process.env.DB_NAME ?? '',
-	server: process.env.DB_HOST ?? '',
-	options: {
-		encrypt: false,
-	},
-	pool: {
-		max: 10,
-		min: 0,
-		idleTimeoutMillis: 30000,
-	},
+  user: process.env.DB_USER ?? '',
+  password: process.env.DB_PWD ?? '',
+  database: process.env.DB_NAME ?? '',
+  server: process.env.DB_HOST ?? '',
+  options: {
+    encrypt: false,
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
 });
 
 export async function selectUser(userEmail: string, provider?: TProvider) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.input('user_email', userEmail)
-		.input('provider', provider)
-		.query(
-			`SELECT * FROM UserTable WHERE user_email = @user_email AND auth_type = @provider`,
-		);
+  return (await sqlConnectionPool.connect())
+    .request()
+    .input('user_email', userEmail)
+    .input('provider', provider)
+    .query(
+      `SELECT * FROM UserTable WHERE user_email = @user_email AND auth_type = @provider`,
+    );
 }
 export async function insertUser(
-	userName: string,
-	userEmail: string,
-	profileImg: string,
-	authType: TProvider,
-	authId: string,
+  userName: string,
+  userEmail: string,
+  profileImg: string,
+  authType: TProvider,
+  authId: string,
 ) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.input('user_name', userName)
-		.input('user_email', userEmail)
-		.input('profile_img', profileImg)
-		.input('auth_type', authType)
-		.input('auth_id', authId).query(`
+  return (await sqlConnectionPool.connect())
+    .request()
+    .input('user_name', userName)
+    .input('user_email', userEmail)
+    .input('profile_img', profileImg)
+    .input('auth_type', authType)
+    .input('auth_id', authId).query(`
 	INSERT INTO UserTable (user_name, user_email, profile_img, auth_type, auth_id, last_login)
 	OUTPUT INSERTED.*
 	VALUES (@user_name, @user_email, @profile_img, @auth_type, @auth_id, GETDATE())
 	`);
 }
 export async function getUserInfoFromSession(session: TExtendedSession | null) {
-	const { recordset } = await selectUser(
-		session?.user?.email ?? '',
-		session?.provider,
-	);
-	console.log('recordset:', recordset);
+  const { recordset } = await selectUser(
+    session?.user?.email ?? '',
+    session?.provider,
+  );
+  console.log('recordset:', recordset);
 
-	console.log('session:', session);
-	if (!recordset.length && session?.user && session?.user?.name) {
-		console.log('new user');
-		const insertedUser = await insertUser(
-			session.user.name as string,
-			session.user.email as string,
-			session.user.image as string,
-			session.provider as TProvider,
-			session.authId as string,
-		);
-		console.log('inserted User:', insertedUser);
-		return insertedUser.recordset[0];
-	} else {
-		return recordset[0];
-	}
+  console.log('session:', session);
+  if (!recordset.length && session?.user && session?.user?.name) {
+    console.log('new user');
+    const insertedUser = await insertUser(
+      session.user.name as string,
+      session.user.email as string,
+      session.user.image as string,
+      session.provider as TProvider,
+      session.authId as string,
+    );
+    console.log('inserted User:', insertedUser);
+    return insertedUser.recordset[0];
+  } else {
+    return recordset[0];
+  }
 }
 export async function insertConv({
-	conversationName,
-	userId,
-	convStringId,
+  conversationName,
+  userId,
+  convStringId,
 }: {
-	conversationName: string;
-	userId: number;
-	convStringId: string;
+  conversationName: string;
+  userId: number;
+  convStringId: string;
 }) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.input('conversation_name', conversationName)
-		.input('user_id', userId)
-		.input('conversation_id', convStringId)
-		.input('salutation', configs.salutationPrefixMessage)
-		.query(`INSERT INTO Conversation (conversation_name, user_id, created_at, status, conversation_id, salutation, visibility) 
-        OUTPUT INSERTED.id VALUES (@conversation_name, @user_id, GETDATE(), 'analyzing', @conversation_id, @salutation, 0)`);
+  return (await sqlConnectionPool.connect())
+    .request()
+    .input('conversation_name', conversationName)
+    .input('user_id', userId)
+    .input('conversation_id', convStringId)
+    .input('salutation', configs.salutationPrefixMessage)
+    .input('question_message', configs.question_message)
+    .query(`INSERT INTO Conversation (conversation_name, user_id, created_at, status, conversation_id, salutation, visibility, question_message) 
+        OUTPUT INSERTED.id VALUES (@conversation_name, @user_id, GETDATE(), 'analyzing', @conversation_id, @salutation, 0, @question_message)`);
 }
 export async function updateConvStatus(
-	convIntId: number,
-	status: TStatus,
-	userId: number,
+  convIntId: number,
+  status: TStatus,
+  userId: number,
 ) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.input('convIntId', convIntId)
-		.input('status', status)
-		.input('user_id', userId).query(`
+  return (await sqlConnectionPool.connect())
+    .request()
+    .input('convIntId', convIntId)
+    .input('status', status)
+    .input('user_id', userId).query(`
 UPDATE Conversation SET status = @status WHERE id = @convIntId AND user_id = @user_id`);
 }
 export async function updateConv({
-	convIntId,
-	userId,
-	newName,
-	newSalutation,
+  convIntId,
+  userId,
+  newName,
+  newSalutation,
 }: {
-	convIntId: number;
-	userId: number;
-	newName: string;
-	newSalutation: string;
+  convIntId: number;
+  userId: number;
+  newName: string;
+  newSalutation: string;
 }) {
-	try {
-		const sqlPool = await sqlConnectionPool.connect();
-		const transaction = sqlPool.transaction();
-		await transaction.begin();
+  try {
+    const sqlPool = await sqlConnectionPool.connect();
+    const transaction = sqlPool.transaction();
+    await transaction.begin();
 
-		// Check if Conversation exists and user_id matches
-		const conversation = await transaction
-			.request()
-			.input('conversation_id', convIntId)
-			.input('user_id', userId).query(`SELECT *
+    // Check if Conversation exists and user_id matches
+    const conversation = await transaction
+      .request()
+      .input('conversation_id', convIntId)
+      .input('user_id', userId).query(`SELECT *
 					FROM Conversation
 					WHERE id = @conversation_id AND user_id = @user_id`);
 
-		if (conversation.recordset.length === 0) {
-			throw new Error('Conversation not found for the given user');
-		}
+    if (conversation.recordset.length === 0) {
+      throw new Error('Conversation not found for the given user');
+    }
 
-		// Update Conversation name
-		await transaction
-			.request()
-			.input('conversation_id', convIntId)
-			.input('conversation_name', newName)
-			.input('salutation', newSalutation)
-			.query(
-				'UPDATE Conversation SET conversation_name=@conversation_name, salutation=@salutation WHERE id=@conversation_id',
-			);
+    // Update Conversation name
+    await transaction
+      .request()
+      .input('conversation_id', convIntId)
+      .input('conversation_name', newName)
+      .input('salutation', newSalutation)
+      .query(
+        'UPDATE Conversation SET conversation_name=@conversation_name, salutation=@salutation WHERE id=@conversation_id',
+      );
 
-		await transaction.commit();
-		console.log('Conversation name updated successfully');
-		return true;
-	} catch (error) {
-		console.error('Error updating conversation name:', error);
-		throw error;
-	}
+    await transaction.commit();
+    console.log('Conversation name updated successfully');
+    return true;
+  } catch (error) {
+    console.error('Error updating conversation name:', error);
+    throw error;
+  }
 }
 // export async function insertDocument({
 // 	documentName,
@@ -212,64 +213,64 @@ export async function updateConv({
 // 	return result;
 // }
 export async function selectConversation(
-	user: TUserFromDB,
-	convStringId: string,
+  user: TUserFromDB,
+  convStringId: string,
 ) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.query(
-			`SELECT * FROM Conversation WHERE conversation_id = '${convStringId}' AND (visibility = 1 OR (visibility = 0 AND user_id = ${user.user_id}))`,
-		);
+  return (await sqlConnectionPool.connect())
+    .request()
+    .query(
+      `SELECT * FROM Conversation WHERE conversation_id = '${convStringId}' AND (visibility = 1 OR (visibility = 0 AND user_id = ${user.user_id}))`,
+    );
 }
 export async function selectConversations(user: TUserFromDB) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.input('user_id', user.user_id)
-		.query(
-			`SELECT * FROM Conversation WHERE user_id = '${user.user_id}' AND status <> 'deleted'`,
-		);
+  return (await sqlConnectionPool.connect())
+    .request()
+    .input('user_id', user.user_id)
+    .query(
+      `SELECT * FROM Conversation WHERE user_id = '${user.user_id}' AND status <> 'deleted'`,
+    );
 }
 export async function selectConvByStr(convStringId: string) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.query(
-			`SELECT * FROM Conversation WHERE conversation_id = '${convStringId}'`,
-		);
+  return (await sqlConnectionPool.connect())
+    .request()
+    .query(
+      `SELECT * FROM Conversation WHERE conversation_id = '${convStringId}'`,
+    );
 }
 export async function selectConvByStrAuth(
-	convStringId: string,
-	userId: number,
+  convStringId: string,
+  userId: number,
 ) {
-	return (await sqlConnectionPool.connect()).request().query(`
+  return (await sqlConnectionPool.connect()).request().query(`
 	SELECT * FROM Conversation WHERE conversation_id = '${convStringId}' AND user_id = ${userId}`);
 }
 export async function selectDocuments(convIntId: number) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.query(`SELECT * FROM Document WHERE conversation_id = '${convIntId}'`);
+  return (await sqlConnectionPool.connect())
+    .request()
+    .query(`SELECT * FROM Document WHERE conversation_id = '${convIntId}'`);
 }
 export async function deleteConversationModel(
-	convIntId: number,
-	userId: number,
+  convIntId: number,
+  userId: number,
 ) {
-	const transaction = sqlConnectionPool.transaction();
-	await transaction.begin();
+  const transaction = sqlConnectionPool.transaction();
+  await transaction.begin();
 
-	//get user id of the conversation
-	const { recordset } = await transaction
-		.request()
-		.input('conversation_id', convIntId)
-		.query(`SELECT user_id FROM Conversation WHERE id=@conversation_id;`);
+  //get user id of the conversation
+  const { recordset } = await transaction
+    .request()
+    .input('conversation_id', convIntId)
+    .query(`SELECT user_id FROM Conversation WHERE id=@conversation_id;`);
 
-	// If the conversation doesn't exist or the user_id doesn't match, throw an error
-	if (recordset.length === 0 || recordset[0].user_id !== userId) {
-		throw new Error(
-			'User does not have permission to delete this conversation',
-		);
-	}
+  // If the conversation doesn't exist or the user_id doesn't match, throw an error
+  if (recordset.length === 0 || recordset[0].user_id !== userId) {
+    throw new Error(
+      'User does not have permission to delete this conversation',
+    );
+  }
 
-	// Delete Conversation
-	await transaction.request().input('conversation_id', convIntId).query(`
+  // Delete Conversation
+  await transaction.request().input('conversation_id', convIntId).query(`
 			DELETE FROM Debate_Message WHERE conversation_id=@conversation_id;
 			DELETE FROM Debate WHERE conversation_id=@conversation_id;
 			DELETE FROM Message WHERE conversation_id=@conversation_id;
@@ -278,22 +279,22 @@ export async function deleteConversationModel(
 			DELETE FROM Question WHERE conversation_id=@conversation_id;
 			DELETE FROM Conversation WHERE id=@conversation_id;
 			`);
-	const result = await transaction.commit();
-	console.log('conversation deleted successfully');
-	return result;
+  const result = await transaction.commit();
+  console.log('conversation deleted successfully');
+  return result;
 }
 export async function selectMessages(convIntId: number, userId: number) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.query(
-			`SELECT * FROM Message WHERE conversation_id = '${convIntId}' AND user_id = ${userId} ORDER BY message_order ASC`,
-		);
+  return (await sqlConnectionPool.connect())
+    .request()
+    .query(
+      `SELECT * FROM Message WHERE conversation_id = '${convIntId}' AND user_id = ${userId} ORDER BY message_order ASC`,
+    );
 }
 export async function selectDebate(answerId: number, userId: number) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.input('answer_id', answerId)
-		.input('user_id', userId).query(`
+  return (await sqlConnectionPool.connect())
+    .request()
+    .input('answer_id', answerId)
+    .input('user_id', userId).query(`
 	SELECT 
 		D.debate_id, 
 		D.question_id, 
@@ -311,15 +312,15 @@ export async function selectDebate(answerId: number, userId: number) {
 		D.answer_id = @answer_id AND D.user_id = @user_id;`);
 }
 export async function selectDebateById(debateId: number) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.input('debate_id', debateId).query(`
+  return (await sqlConnectionPool.connect())
+    .request()
+    .input('debate_id', debateId).query(`
 	SELECT * FROM Debate WHERE debate_id = @debate_id`);
 }
 export async function selectDebateMessages(debateId: number, userId: number) {
-	return (await sqlConnectionPool.connect())
-		.request()
-		.query(
-			`SELECT * FROM Debate_Message WHERE debate_id = ${debateId} AND user_id = ${userId} ORDER BY id ASC`,
-		);
+  return (await sqlConnectionPool.connect())
+    .request()
+    .query(
+      `SELECT * FROM Debate_Message WHERE debate_id = ${debateId} AND user_id = ${userId} ORDER BY id ASC`,
+    );
 }
