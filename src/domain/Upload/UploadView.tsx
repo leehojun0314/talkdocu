0be09d7c789fd2fbc4cd96a42d66530d3575aed3
leehojun0314 <dlghwns0314@naver.com>
2024-11-21 12:57:49 +1000

    remove alanytics

--- src/domain/Upload/UploadView.tsx
+++ src/domain/Upload/UploadView.tsx
@@ -3,34 +3,34 @@ import { useCustomMediaQuery } from '@/common/theme/screen';
 import { css } from '@emotion/react';
 
 import {
-	FirstSection,
-	MainSection,
-	MobileMainSection,
-	SecondSection,
-	ThirdSection,
+  FirstSection,
+  MainSection,
+  MobileMainSection,
+  SecondSection,
+  ThirdSection,
 } from './sections';
-import { texts, enTexts } from './model/texts';
+import { enTexts } from './model/texts';
 export type Ttext = {
-	bigTitle: string;
-	rightText: string;
-	leftTitle: string;
-	leftText: string;
+  bigTitle: string;
+  rightText: string;
+  leftTitle: string;
+  leftText: string;
 };
 export const UploadView = () => {
-	const { isSmall } = useCustomMediaQuery();
-	return (
-		<div css={sx.root}>
-			{isSmall ? <MobileMainSection /> : <MainSection />}
-			<FirstSection text={enTexts[0]} />
-			<SecondSection text={enTexts[1]} />
-			<ThirdSection text={enTexts[2]} />
-			<PcFooter position='relative' />
-		</div>
-	);
+  const { isSmall } = useCustomMediaQuery();
+  return (
+    <div css={sx.root}>
+      {isSmall ? <MobileMainSection /> : <MainSection />}
+      <FirstSection text={enTexts[0]} />
+      <SecondSection text={enTexts[1]} />
+      <ThirdSection text={enTexts[2]} />
+      <PcFooter position='relative' />
+    </div>
+  );
 };
 
 const sx = {
-	root: css`
-		height: 100%;
-	`,
+  root: css`
+    height: 100%;
+  `,
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,51 +8,51 @@ import { useEffect } from 'react';
 import axiosAPI from '@/utils/axiosAPI';
 import getConfig from 'next/config';
 const { NODE_ENV_CLI } = getConfig().publicRuntimeConfig;
-import { Analytics } from '@vercel/analytics/react';
+// import { Analytics } from '@vercel/analytics/react';
 import { SessionProvider } from 'next-auth/react';
 
 export default function App({
-	Component,
-	pageProps: { session, ...pageProps },
+  Component,
+  pageProps: { session, ...pageProps },
 }: AppProps) {
-	function googleTagManager(w: any, d: any, s: any, l: any, i: any) {
-		w[l] = w[l] || [];
-		w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-		var f = d.getElementsByTagName(s)[0],
-			j = d.createElement(s),
-			dl = l != 'dataLayer' ? '&l=' + l : '';
-		j.async = true;
-		j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-		f.parentNode.insertBefore(j, f);
-	}
-	useEffect(() => {
-		googleTagManager(window, document, 'script', 'dataLayer', 'GTM-WBB4N3M');
-		// if (NODE_ENV_CLI === 'development') {
-		// 	axiosAPI({
-		// 		url: '/test/',
-		// 		method: 'GET',
-		// 	})
-		// 		.then((response) => {
-		// 			// console.log('check response: ', response.data);
-		// 		})
-		// 		.catch((err) => {
-		// 			console.log('err: ', err);
-		// 			window.alert(
-		// 				'The test server has been shut down. Please use the production page below. \n https://talkdocu.com',
-		// 			);
-		// 			window.location.href = 'https://www.talkdocu.com';
-		// 		});
-		// }
-	}, []);
-	return (
-		<ThemeProvider theme={lightTheme}>
-			<CssBaseline />
-			<Provider store={store}>
-				<SessionProvider session={session}>
-					<Component {...pageProps} />
-				</SessionProvider>
-				<Analytics />
-			</Provider>
-		</ThemeProvider>
-	);
+  function googleTagManager(w: any, d: any, s: any, l: any, i: any) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  }
+  useEffect(() => {
+    googleTagManager(window, document, 'script', 'dataLayer', 'GTM-WBB4N3M');
+    // if (NODE_ENV_CLI === 'development') {
+    // 	axiosAPI({
+    // 		url: '/test/',
+    // 		method: 'GET',
+    // 	})
+    // 		.then((response) => {
+    // 			// console.log('check response: ', response.data);
+    // 		})
+    // 		.catch((err) => {
+    // 			console.log('err: ', err);
+    // 			window.alert(
+    // 				'The test server has been shut down. Please use the production page below. \n https://talkdocu.com',
+    // 			);
+    // 			window.location.href = 'https://www.talkdocu.com';
+    // 		});
+    // }
+  }, []);
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Provider store={store}>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+        {/* <Analytics /> */}
+      </Provider>
+    </ThemeProvider>
+  );
 }
